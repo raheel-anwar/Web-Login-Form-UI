fe79f7c25fda54ad5a4aaa880433ade104900038
Raheel Anwar <raheelanwar74@gmail.com>
2017-02-03 20:19:29 +0500

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,27 @@
+var user = document.getElementById("username");
+var pass = document.getElementById("password");
+
+/*======Username field autofill events======*/
+user.onfocus = function () {
+	if (user.value === "Username") {
+		user.value = "";
+	}
+};
+user.onblur = function () {
+	if (user.value === "") {
+		user.value = "Username";
+	}
+};
+/*==========================================*/
+/*======Password field autofill events======*/
+pass.onfocus = function () {
+	if (pass.value === "Password") {
+		pass.value = "";
+	}
+};
+pass.onblur = function () {
+	if (pass.value === "") {
+		pass.value = "Password";
+	}
+};
+/*==========================================*/
