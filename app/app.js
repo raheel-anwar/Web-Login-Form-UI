8ec91e8c161de7da59492ba11d0703e7588dd671
Raheel Anwar <raheelanwar74@gmail.com>
2017-02-05 11:29:45 +0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,27 +1,33 @@
-var user = document.getElementById("username");
-var pass = document.getElementById("password");
+function formDefaultText() {
+	var user = document.getElementById("username");
+	var pass = document.getElementById("password");
 
-/*======Username field autofill events======*/
-user.onfocus = function () {
-	if (user.value === "Username") {
-		user.value = "";
-	}
-};
-user.onblur = function () {
-	if (user.value === "") {
-		user.value = "Username";
-	}
-};
-/*==========================================*/
-/*======Password field autofill events======*/
-pass.onfocus = function () {
-	if (pass.value === "Password") {
-		pass.value = "";
-	}
-};
-pass.onblur = function () {
-	if (pass.value === "") {
-		pass.value = "Password";
-	}
-};
-/*==========================================*/
+	/*======Username field autofill events======*/
+	user.onfocus = function () {
+		if (user.value === "Username") {
+			user.value = "";
+		}
+	};
+	user.onblur = function () {
+		if (user.value === "") {
+			user.value = "Username";
+		}
+	};
+	/*==========================================*/
+	/*======Password field autofill events======*/
+	pass.onfocus = function () {
+		if (pass.value === "Password") {
+			pass.value = "";
+		}
+	};
+	pass.onblur = function () {
+		if (pass.value === "") {
+			pass.value = "Password";
+		}
+	};
+	/*==========================================*/
+}
+
+window.onload = function () {
+	formDefaultText();
+}
